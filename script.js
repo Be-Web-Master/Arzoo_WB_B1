325d736bf0d3ed19ef6451d50f8460ef4be1f607
Arzoo-25 <ajarzoo25@gmail.com>
2023-08-16 02:44:18 +0530

    completed object method practice sheet

--- script.js
+++ script.js
@@ -152,3 +152,128 @@
 // delete obj.name
 // console.log(obj)
 
+// 11. Create an object and check for isSealed after applying .seal and without applying .seal
+
+// prevents extension, new properties cannot be added, existing properties cannot be removed,Values of existing properties can still be changed,prototype cannot be re-assigned,prevents extension, new properties cannot be added, existing properties cannot be removed,Values of existing properties can still be changed,prototype cannot be re-assigned,
+
+// const obj = {
+//         name:"Arzoo",
+//         status:"Graduate"
+//     };
+//     // Object.seal(obj)
+//     Object.defineProperties(obj,{
+//         city:{
+//             value: "Bhopal",
+//             writable: true
+//         },
+//         state: {
+//             value:"M.P.",
+//             writable:true
+//         }
+//     })
+// // delete obj.name
+// console.log(obj);
+// console.log(Object.isSealed(obj))
+
+// 12. Create an object and check for isExtensible after applying .preventExtensions and without applying .preventExtensions
+
+// prevents adding new properties, prevents the object's prototype from being re-assigned
+// const obj = {
+//         name:"Arzoo",
+//         status:"Graduate"
+//     };
+//     // Object.preventExtensions(obj)
+//     Object.defineProperties(obj,{
+//         city:{
+//             value: "Bhopal",
+//             writable: true
+//         },
+//         state: {
+//             value:"M.P.",
+//             writable:true
+//         }
+//     })
+// Object.defineProperty(obj, 'city', {value: "Sagar"})
+// console.log(obj);
+// console.log(Object.isExtensible(obj))
+
+// 13. Create an object and check for isFrozen after applying .freeze and without applying .freeze
+
+// prevents extension, new properties cannot be added, existing properties cannot be removed,Values of existing properties CAN NOT be changed,prototype cannot be re-assigned
+
+// const obj = {
+//     name:"Arzoo",
+//     status:"Graduate"
+// };
+// // Object.freeze(obj)
+// Object.defineProperties(obj,{
+//     city:{
+//         value: "Bhopal",
+//         writable: true
+//     },
+//     state: {
+//         value:"M.P.",
+//         writable:true
+//     }
+// })
+// Object.defineProperty(obj, 'city', {value: "Sagar"})
+// delete obj.status;
+// console.log(obj);
+// console.log(Object.isFrozen(obj));
+
+// 14. Create an object and gets it's prototype
+
+// const obj = {
+//     item: "Samsung Z Flip",
+//     origin: "South Korea",
+//     flexible: true,
+//     touch: true
+// }
+// console.log(obj)
+// console.log(Object.getPrototypeOf(obj))
+
+// 15. Create two objects obj1, obj2 and set obj1’s prototype on the obj2 object and try to get obj1 properties through obj2, which should not exist in obj2 before.
+
+// const obj1 ={
+//     item: "Samsung ZFlip",
+//     origin: "South Korea",
+//     flexible: true,
+//     touch: true
+// }
+// const obj2 ={
+//     item: "Samsung ZFold",
+//     origin: "South Korea"
+// }
+// Object.setPrototypeOf(obj2,obj1)
+// console.log(obj2.touch)
+// console.log(obj2.flexible) 
+
+// 16.  Create an object and apply .entries then create a new object using .fromEntries() and assign it in myObj2.
+// entries()-> convert obj to map
+// const obj1 = {
+//     item: "Samsung ZFlip",
+//     origin: "South Korea",
+//     flexible: true,
+//     releaseDate: "July 26"
+// }
+// console.log(Object.entries(obj1));
+// fromentries()-> convert map to obj
+// const entries = new Map([
+//     ['album','Lover'],
+//     ['singer','Taylor Swift']
+// ]);
+// const myObj2 = Object.fromEntries(entries);
+// console.log(myObj2)
+
+// 17. Create an object and try to check two properties using .hasOwnProperty, second property should not exist.
+const obj1 = {
+    item: "Samsung ZFlip",
+    origin: "South Korea",
+    flexible: true,
+    releaseDate: "July 26"
+}
+console.log(Object.hasOwnProperty(obj1,"flexible"))
+console.log(Object.hasOwn(obj1,"flexible"))
+// hasOwn is printing true if the property is correct
+// hasOwnProperty is printing false. why???
+// console.log(obj1)
