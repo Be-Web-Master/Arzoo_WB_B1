e212924b580a7c65961a84d5a9b4961b248155f9
Arzoo-25 <ajarzoo25@gmail.com>
2023-08-21 15:26:09 +0530

    COMPLETED!

--- day-19/question22.js
+++ day-19/question22.js
@@ -1,7 +1,17 @@
 // 22. Create a program that counts the occurrence of each character in a given string and displays the character count
-function checkOccurrence(str){
-    let counter = [];
-    
+let str = "hi my name is arzoo";
+function checkOccurrence(str,char){
+    let counter = {};
+    let newStr = str.split("");
+    for(let i=0;i<newStr.length;i++){
+        if(counter[newStr[i]]){
+            counter[newStr[i]] = counter[newStr[i]]+1;
+        }
+        else{
+            counter[newStr[i]] = 1;
+        }
+    }
+    return{counter, char: counter[char]}
 
 }
-checkOccurrence("hi my name is arzoo")
+console.log(checkOccurrence(str,'h'))
