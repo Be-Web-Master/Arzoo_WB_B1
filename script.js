05773e0957c3d8959e313de80987f48439e3800e
Arzoo-25 <ajarzoo25@gmail.com>
2023-08-27 12:32:55 +0530

    table on local storage

--- script.js
+++ script.js
@@ -1,163 +1,138 @@
 // const table = document.getElementById("table");
 const thead = document.getElementById("thead");
 const tbody = document.getElementById("tbody");
-
-const Table = {
-  header: [
-    {
-      element: "th",
-      columnName: "Column A",
-      order: 1,
-      sort: undefined,
-    },
-    {
-      element: "th",
-      columnName: "Column B",
-      order: 2,
-      sort: undefined,
-    },
-    {
-      element: "th",
-      columnName: "Column C",
-      order: 3,
-      sort: undefined,
-    },
-  ],
-  body: [
-    {
-      element: "tr",
-      rowOrder: 1,
-      columnElements: [
-        {
-          elemType: "td",
-          columnName: "Column A",
-          rowOrder: 1,
-          value: "row1",
-        },
-        {
-          elemType: "td",
-          columnName: "Column B",
-          rowOrder: 1,
-          value: "row1",
-        },
-        {
-          elemType: "td",
-          columnName: "Column C",
-          rowOrder: 1,
-          value: "row1",
-        },
-      ],
-    },
-    {
-      element: "tr",
-      rowOrder: 2,
-      columnElements: [
-        {
-          elemType: "td",
-          columnName: "Column A",
-          rowOrder: 2,
-          value: "row2",
-        },
-        {
-          elemType: "td",
-          columnName: "Column B",
-          rowOrder: 2,
-          value: "row2",
-        },
-        {
-          elemType: "td",
-          columnName: "Column C",
-          rowOrder: 2,
-          value: "row2",
-        },
-      ],
-    },
-    {
-      element: "tr",
-      rowOrder: 3,
-      columnElements: [
-        {
-          elemType: "td",
-          columnName: "Column A",
-          rowOrder: 3,
-          value: "row3",
-        },
-        {
-          elemType: "td",
-          columnName: "Column B",
-          rowOrder: 3,
-          value: "row3",
-        },
-        {
-          elemType: "td",
-          columnName: "Column C",
-          rowOrder: 3,
-          value: "row3",
-        },
-      ],
-    },
-  ],
+const columnBtn = document.getElementById("columnBtn");
+columnBtn.onclick = addNewColoumn;
+const rowBtn = document.getElementById("rowBtn");
+rowBtn.onclick = addNewRow;
+const clrBtn = document.getElementById("clear");
+clrBtn.onclick = () => {
+  localStorage.clear();
 };
 
-const columnBtn = document.getElementById("columnBtn");
-columnBtn.onclick = addColoumnHead;
-const rowBtn = document.getElementById("rowBtn");
-rowBtn.onclick = addRowData;
+thead.addEventListener("keyup", (event) => {
+  console.log(event.target);
+});
 
-function addColoumnHead() {
-  const th = document.createElement("th");
-  thead.append(th);
-  const columnCount = thead.childElementCount;
-  th.innerText = `Column ${columnCount}`;
-  th.style.border = "1px solid #000";
-  // console.log(tbody.childElementCount)
-  if (tbody.childElementCount) {
-    for (const elem of tbody.children) {
-      // console.log(elem)
-      const td = document.createElement("td");
-      elem.appendChild(td);
-      td.append(createRowInput());
-    }
-  }
+tbody.addEventListener("keyup", (event) => {
+  console.log(event.target);
+});
+
+const LOCAL_STORAGE_TABLE_KEY = "tableStateData";
+
+let tableState = {
+  header: [],
+  body: [],
+};
+
+// to store table data in local Storage
+if (localStorage.getItem(LOCAL_STORAGE_TABLE_KEY)) {
+  // agr data milta hai to tableState ma override kr denge
+  tableState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TABLE_KEY));
+} else {
+  // vrna data set kr denge
+  localStorage.setItem(LOCAL_STORAGE_TABLE_KEY, JSON.stringify(tableState));
 }
 
-function addRowData() {
-  const tr = document.createElement("tr");
-  tbody.appendChild(tr);
-  for (let j = 1; j <= thead.children.length; j++) {
-    const td = document.createElement("td");
-    tr.appendChild(td);
-    td.append(createRowInput());
-  }
-  // tr.style.backgroundColor = "yellow"
+function updateTableData() {
+  localStorage.setItem(LOCAL_STORAGE_TABLE_KEY, JSON.stringify(tableState));
 }
 
-function createRowInput() {
-  const input = Object.assign(document.createElement("input"), {
-    type: "text",
-    placeholder: "Enter row",
+function addNewColoumn() {
+  const headerObj = {
+    elementType: "th",
+    columnName: "",
+    columnNumber: tableState.header.length,
+    sort: undefined,
+  };
+  tableState.header.push(headerObj);
+  tableState.body.forEach((rowObj, idx) => {
+    rowObj.cellDetails.push({
+      elemType: "td",
+      columnNumber: "",
+      rowNumber: idx + 1,
+      cellValue: "",
+    });
   });
-
-  // input.setAttribute("type","text")
-  // input.setAttribute("placeholder","Enter row")
-  return input;
+  createTableUI(tableState);
 }
 
-function storeTableData() {
-  for (const columnText of Table.header) {
-    const th = document.createElement(columnText.element);
-    th.innerText = columnText.columnName;
+function addNewRow() {
+
+    const bodyObj = {
+      elementType: "tr",
+      rowNumber: tableState.body.length + 1,
+      cellDetails: [],
+    };
+    tableState.header.forEach((headElem) => {
+        // console.log(headElem)
+      bodyObj.cellDetails.push({
+        elemType: "td",
+        columnNumber: headElem.columnNumber +1,
+        rowNumber: tableState.body.length + 1,
+        cellValue: "",
+      });
+    });
+    tableState.body.push(bodyObj);
+    createTableUI(tableState);
+  }
+
+
+function createTableUI() {
+  thead.innerHTML = "";
+  tbody.innerHTML = "";
+  // to create column
+  for (const columnText of tableState.header) {
+    const th = document.createElement(columnText.elementType);
+    console.log(columnText);
+    const columnNumber = columnText.columnNumber;
+    const columnInput = createColumnInput(columnNumber);
+    columnInput.value = columnText.columnName;
+    // th.innerText = columnText.columnName;
+    th.append(columnInput);
     thead.append(th);
   }
-  for (const rowText of Table.body) {
-    const tr = document.createElement(rowText.element);
-    for (const rowData of rowText.columnElements) {
-      const td = tr.appendChild(
-        document.createElement(rowData.elemType)
-      );
-      td.innerText = rowData.value; 
+  //   to add row value
+  for (const rowText of tableState.body) {
+    const tr = document.createElement(rowText.elementType);
+    for (const rowData of rowText.cellDetails) {
+        console.log(rowData)
+      const rowNumber = rowData.rowNumber;
+      const columnNumber = rowData.columnNumber;
+      const td = document.createElement(rowData.elemType);
+      const inputElement = createRowInput(rowNumber,columnNumber);
+      //   inputElement.setAttribute("value", rowData.value);
+
+      inputElement.value = rowData.cellValue;
+      // console.log(inputElement.value)
+      td.append(inputElement);
+      tr.append(td);
     }
     tbody.append(tr);
   }
+  //   jb ui dikha rhe hai tabhi localStorage funciton ko call kr denge
+  updateTableData();
 }
-storeTableData();
+createTableUI();
+
+
+
+function createColumnInput(columnNumber) {
+  const input = Object.assign(document.createElement("input"), {
+    id: `0-${columnNumber}`,
+    type: "text",
+    placeholder: "Enter Column",
+  });
+  return input;
+}
+
+function createRowInput(rowNumber, columnNumber) {
+    console.log(`${rowNumber}-${columnNumber}`)
+  const input = Object.assign(document.createElement("textarea"), {
+    id: `${rowNumber}-${columnNumber}`,
+    row: "10",
+    column: "10",
+    placeholder: "Enter row",
+  });
+  return input;
+}
