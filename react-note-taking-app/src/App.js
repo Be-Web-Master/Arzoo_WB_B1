40427ff3d3590daa2382843f6cc361136dcaf97f
Arzoo-25 <ajarzoo25@gmail.com>
2023-09-07 02:59:32 +0530

    remaining edit modal and local storage

---
+++ react-note-taking-app/src/App.js
@@ -0,0 +1,71 @@
+import './App.css';
+import {useState} from "react"
+function NoteTakingApp() {
+  const [title, setTitle] = useState("")
+  const [disc, setDisc] = useState("")
+  const [notes, setNotes] = useState([])
+  const [modal, setModal] = useState(false)
+
+  const handleSubmit = () =>{
+    const newNotes = {title,disc};
+    setNotes([...notes,newNotes]);
+    setTitle('')
+    setDisc('')
+    console.log(newNotes)
+  }
+  const deleteNotes = (idx) =>{
+     setNotes((notes)=>{
+      return notes.filter((note,id)=>{
+        return id !== idx
+      })
+     })
+  }
+  const editNotes =(idx) =>{
+     setTitle(notes[idx].title);
+     setDisc(notes[idx].disc);
+     setModal(true);
+  }
+  return (
+    <div>
+      <h1>Create Notes</h1>
+      <form 
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}>
+        <input className='input' type="text" placeholder="Title" value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
+        <textarea placeholder='Discription' cols="50" rows="4" value={disc} onChange={(e)=>{setDisc(e.target.value)}}></textarea>
+        <button className='btn' type="submit"  onClick={handleSubmit}>Submit</button>
+      </form>
+      <hr />
+      <div className="Notes">
+         {notes.map((notes,idx)=>{
+            return <div className="note" key={idx}>
+              <h1 className='h1'>
+                {notes.title}
+                <span className='delete' onClick={()=>deleteNotes(idx)}>❌</span>
+                <span className='edit' onClick={()=>editNotes(idx)}>✏️</span>
+              </h1>
+              <h2 className='h2'>{notes.disc}</h2>
+            </div>
+         })}
+      </div>
+
+      {modal && <div>
+        <h1>Edit Notes</h1>
+        <form 
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}>
+        <input className='input' type="text" placeholder="Title" value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
+        <textarea placeholder='Discription' cols="50" rows="4" value={disc} onChange={(e)=>{setDisc(e.target.value)}}></textarea>
+        <button className='btn' type="submit"  onClick={handleSubmit}>Submit</button>
+      </form>
+      </div>
+      }
+    </div>
+  );
+}
+
+export default NoteTakingApp;
