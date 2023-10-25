371273ba14b0133c5fa132c9e7dd3700514213fc
Arzoo-25 <ajarzoo25@gmail.com>
2023-10-25 14:18:32 +0530

    remaining

---
+++ que1.js
@@ -0,0 +1,8 @@
+const arr =[1,2,3,4,5];
+function myReduce(arr,initialVal,accumulator,currVal){
+arr.forEach(elem => {
+    initialVal ? initialVal = arr[0] : initialVal = arr[1];
+    
+});
+}
+myReduce(arr)
