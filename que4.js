62809551336b4cfc5d9b65f3225aafe5c03ee11d
Arzoo-25 <ajarzoo25@gmail.com>
2023-10-30 00:29:56 +0530

    All done.

--- que4.js
+++ que4.js
@@ -3,12 +3,19 @@
 // Notice that the solution set must not contain duplicate triplets.
 
 const nums = [-1,0,1,2,-1,-4];
-const res = [];
 
-function duplicateTriplets(nums){
-let sum = 0;
+function duplicateTriplets(nums,target){
+const res = [];
 for(let i=0; i<nums.length; i++){
-    
+    for(let j=i+1; j<nums.length; j++){
+        for(let k= j+1; k<nums.length;k++){
+            if(nums[i] + nums[j] + nums[k] === target){
+                console.log({i,j,k});
+                res.push(nums[i],nums[j],nums[k]);
+                return res;
+            }
+        }
+    }
 }
 }
-console.log(duplicateTriplets(nums));
+console.log(duplicateTriplets(nums,0));
